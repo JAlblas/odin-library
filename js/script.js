2bb31b6aabe2d139dafdd6a1bcdaac0a1aa0aa48
Jasper Alblas <just_jasper@hotmail.com>
2024-07-09 22:17:33 +0200

    Adds functionality to show books on page

--- js/script.js
+++ js/script.js
@@ -1,14 +1,37 @@
-const myLibrary = [];
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.info = function () {
+    console.log(`${this.title}, ${this.author}, ${this.pages}, ${this.read}`);
+}
+
+const myLibrary = [
+    new Book("Harry Potter 1", "J.K.K. Rowling", 576, true)
+];
+
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    books.push(book);
+}
+
+function loadContent() {
+    const booksDiv = document.querySelector('.books');
+    booksDiv.innerHTML = "";
+    myLibrary.forEach(book => {
+        booksDiv.append(`${book.title}`);
+    });
+
+}
 
 const dialog = document.querySelector('dialog');
-dialog.showModal();
+//dialog.showModal();
 
-console.log("TEST)");
+loadContent();
 
-function Book() {
-    // the constructor...
-}
 
-function addBookToLibrary() {
-    // do stuff here
-}
+
+
