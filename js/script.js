fc7011e511e73a4eafe319e3eaf0fa3de9fba415
Jasper Alblas <just_jasper@hotmail.com>
2024-07-10 09:28:11 +0200

    Book info now added to page with proper structure

--- js/script.js
+++ js/script.js
@@ -10,7 +10,12 @@ Book.prototype.info = function () {
 }
 
 const myLibrary = [
-    new Book("Harry Potter 1", "J.K.K. Rowling", 576, true)
+    new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 223, true),
+    new Book("A Tale of Two Cities", "Charles Dickens", 448, false),
+    new Book("The Alchemist", "Paulo Coelho", 163, false),
+    new Book("The Hobbit", "J.R.R. Tolkien", 310, true),
+    new Book("And Then There Were None", "Agatha Christie", 272, false),
+    new Book("The Da Vinci Code", "Dan Brown", 689, true)
 ];
 
 function addBookToLibrary(title, author, pages, read) {
@@ -22,7 +27,29 @@ function loadContent() {
     const booksDiv = document.querySelector('.books');
     booksDiv.innerHTML = "";
     myLibrary.forEach(book => {
-        booksDiv.append(`${book.title}`);
+        const bookDiv = document.createElement('div');
+        bookDiv.classList.add('book');
+        const bookTitle = document.createElement('h4');
+        bookTitle.classList.add('title');
+        bookTitle.innerText = book.title;
+        bookDiv.appendChild(bookTitle);
+
+        const bookAuthor = document.createElement('p');
+        bookAuthor.classList.add('author');
+        bookAuthor.innerText = book.author;
+        bookDiv.appendChild(bookAuthor);
+
+        const bookPages = document.createElement('p');
+        bookPages.classList.add('pages');
+        bookPages.innerText = `${book.pages} pages`;
+        bookDiv.appendChild(bookPages);
+
+        const bookRead = document.createElement('p');
+        bookRead.classList.add('read');
+        bookRead.innerText = book.read ? "Book read" : "Book not read";
+        bookDiv.appendChild(bookRead);
+
+        booksDiv.append(bookDiv);
     });
 
 }
