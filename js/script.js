b8523601801fdc23ebbb9cd7ee6aa1ac8b4f692c
Jasper Alblas <just_jasper@hotmail.com>
2024-07-25 14:13:10 +0200

    Changes Book to class syntax instead of plain constructors

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-Book.prototype.toggleRead = function () {
-    this.read = !this.read;
+    toggleRead() {
+        this.read = !this.read;
+    }
 }
 
 const myLibrary = [
